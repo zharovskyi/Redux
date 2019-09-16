f146fd424650badc376e84babac96de7f916639f
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-09-16 18:31:13 +0300

    add change ignore

---
+++ redax/build/precache-manifest.d5ab10ec53fc7cab0a646aefe87e9830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd833663679d0023a1b121fc76f20296",
+    "url": "/Redux/redax/build/index.html"
+  },
+  {
+    "revision": "11aa93e3ef1fdede0eea",
+    "url": "/Redux/redax/build/static/css/main.951a195d.chunk.css"
+  },
+  {
+    "revision": "dfb4ae8f5a2ec0a46a75",
+    "url": "/Redux/redax/build/static/js/2.8d2a667c.chunk.js"
+  },
+  {
+    "revision": "11aa93e3ef1fdede0eea",
+    "url": "/Redux/redax/build/static/js/main.bb836da5.chunk.js"
+  },
+  {
+    "revision": "229ef35bb11c64ee367e",
+    "url": "/Redux/redax/build/static/js/runtime~main.89750d05.js"
+  }
+]);
